8d3f06901842a633a51461bb5ba8dd76c05e0bae
unknown <Xells>
2015-11-06 23:25:56 +0100

    "First real commit"

---
+++ StrangeRituals/Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSwitch : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "LevelSwitch")
+        {
+            Application.LoadLevel("LvL2");
+        }
+    }
+}
---
+++ StrangeRituals/Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float LookAtSpeed = 1;
+    public float JumpPower = 1;
+
+    [HideInInspector]
+    public bool Grounded = true;
+
+    bool jump = false;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Vertical");
+        float MouseX = Input.GetAxis("Mouse X");
+        float z = Input.GetAxis("Horizontal");
+
+        if (Input.GetButton("LookRight"))
+            transform.Rotate(0, LookAtSpeed, 0, Space.Self);
+
+        if (Input.GetButton("LookLeft"))
+            transform.Rotate(0, -LookAtSpeed, 0, Space.Self);
+
+        transform.Translate(x * Time.deltaTime * 10, 0, -z * Time.deltaTime * 10);
+        transform.Rotate(0, MouseX, 0);
+
+        if (Input.GetButtonDown("Jump") & !jump)
+            transform.Translate(0, JumpPower, 0);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Grounded = true;
+    }
+    void OnCollisionExit(Collision other)
+    {
+        Grounded = false;
+    }
+
+}
